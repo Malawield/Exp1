f6ac83e2f260eca6493b95ff81f583c92a761d3f
Malawield <Malawield808@mail.ru>
2024-08-11 02:37:41 +0700

    Add files via upload

---
+++ module_1_2.py
@@ -0,0 +1,5 @@
+homework_count = 12
+hours_count = 1.5
+cours_name = 'Python'
+time_for_tesk = hours_count / homework_count
+print('Курс:', cours_name, ',всего задач:', homework_count, ',затрачено часов:', hours_count, ',среднее время выполнения:', time_for_tesk, 'часа.')
