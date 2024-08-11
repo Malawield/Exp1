0b425d27c3d233b1e810533fb97d6c9501674582
Malawield <Malawield808@mail.ru>
2024-08-12 01:37:26 +0700

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,6 @@
+immutable_var = 'Denis', 1, False, 1.5
+print(immutable_var)
+#immutable_var[0] = 'Igor'   # кортеж неизменяемый список
+mutable_list = ['Denis', 1, False, 1.5]
+mutable_list[0] = 'Igor'
+print(mutable_list)
